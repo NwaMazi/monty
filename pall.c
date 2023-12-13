a991f706f544eae746419cca90423767c332a266
Vitalis Nwosu <nwamazi1785@gmail.com>
2023-12-13 08:15:49 -0800

    monty task 0

--- pall.c
+++ pall.c
@@ -1,5 +1,7 @@
 #include "monty.h"
-/** F_pall - this code prints all values in stack
+
+/**
+ * F_pall - this code prints all values in stack
  * @head: points to the stack
  */
 
@@ -8,6 +10,7 @@ void F_pall(stack_t **head)
 	stack_t *current = *head;
 
 	while (current != NULL)
+
 	{
 		printf("%d\n", current->n);
 		current = current->next;
